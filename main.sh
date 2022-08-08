668f146b49be9625c83598a68bcc2a3fcc06d8e0
Zackery .R. Smith <72983221+ZackeryRSmith@users.noreply.github.com>
2022-08-08 15:54:45 -0400

    Added error handling if "sensors" does not exist

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 clear
-while true;
-do
+if ! command -v sensors &> /dev/null; then
+    echo "The command \"sensors\" is not found. Please install the lm-sensors package!" && exit
+fi
 
+while true; do
 	sensors | grep -Eo "Core [0-9]+:\s+\+[0-9]+\.[0-9]°\S+"  
-	sleep 1
-	clear
+	sleep 1 && clear
 done
-
